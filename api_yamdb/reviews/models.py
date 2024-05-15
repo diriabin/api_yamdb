1a8382dc16a242596506658a980f9289378e3128
Семён Поркулевич <sadd9d9@mail.ru>
2024-05-15 14:58:02 +0600

    make model title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Title(models.Model):
+    name = models.CharField(unique=True, max_length=256, verbose_name='Название')
+    year = models.IntegerField(verbose_name='Год выпуска')
+    description = models.CharField(verbose_name='Описание')
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.SET_NULL,
+        verbose_name='Жанры',
+        related_name='titles'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name[:20]
