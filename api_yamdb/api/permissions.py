c95e58589591028088917233d68d754570be12a6
Семён Поркулевич <sadd9d9@mail.ru>
2024-05-15 21:34:10 +0600

    add url, serializer, permission view for genre and fix title

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,3 +9,13 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin')
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin')
