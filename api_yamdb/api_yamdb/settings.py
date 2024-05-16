8c19ae782806d7a33e9fa715315d3671cbcbd819
Семён Поркулевич <sadd9d9@mail.ru>
2024-05-16 13:36:12 +0600

    add url, view, serializer for categories; add users in apps

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'users.apps.UsersConfig',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig'
 ]
