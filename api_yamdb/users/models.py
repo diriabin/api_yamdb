a8520e69a57e152ed72baec8c4499adaf4cd728f
Artem Diriabin <artem.diriabin@yandex.ru>
2024-05-16 13:35:25 +0700

    clean customuser model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -5,12 +5,15 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(verbose_name='биография', null=True, blank=True)
     role = models.CharField(
-        default='user', choices=(
+        default='user',
+        choices=(
             ('user', 'user'),
             ('moderator', 'moderator'),
             ('admin', 'admin'),
-        ), verbose_name='роль', max_length=10
+        ),
+        verbose_name='роль',
+        max_length=10
     )
 
     def __str__(self):
-        return self.username
+        return self.username[:20]
