c01b30c5e4c9c5302be10c1cad66db996506c1cc
Семён Поркулевич <sadd9d9@mail.ru>
2024-05-17 15:26:03 +0600

    delete validate_slug for genre and category

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,3 @@
-import re
-
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
@@ -8,8 +6,6 @@ from reviews.models import Category, Genre, Title
 from users.models import СonfirmationСode
 
 User = get_user_model()
-
-SLUG_PATTERN = r'^[-a-zA-Z0-9_]+$'
 
 
 class TitleSerializer(serializers.ModelSerializer):
@@ -26,26 +22,12 @@ class GenreSerializer(serializers.ModelSerializer):
         model = Genre
         fields = ('name', 'slug')
 
-    def validate_slug(self, value):
-        if re.fullmatch(SLUG_PATTERN, value):
-            return value
-        raise serializers.ValidationError(
-            'Для слага можно использовать только: A-Z, a-z, 0-9, _'
-        )
-
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
         fields = ('name', 'slug')
-
-    def validate_slug(self, value):
-        if re.fullmatch(SLUG_PATTERN, value):
-            return value
-        raise serializers.ValidationError(
-            'Для слага можно использовать только: A-Z, a-z, 0-9, _'
-        )
 
 
 class UserSerializer(serializers.ModelSerializer):
