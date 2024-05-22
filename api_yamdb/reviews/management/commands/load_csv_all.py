0945e869c2610740d8d0c07ede60e3c929c4f86f
Nikita Maslenko <nickita0098@gmail.com>
2024-05-22 11:20:16 +0200

    изменил модель Titles, добавил одну комманду для импорта всех csv

---
+++ api_yamdb/reviews/management/commands/load_csv_all.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand, call_command
+
+class Command(BaseCommand):
+    help = 'Загрузка данных из всех CSV файлов'
+
+    def handle(self, *args, **kwargs):
+        commands = [
+            ('load_csv_users', 'static/data/users.csv'),
+            ('load_csv_category', 'static/data/category.csv'),
+            ('load_csv_genre', 'static/data/genre.csv'),
+            ('load_csv_titles', 'static/data/titles.csv'),
+            ('load_csv_review', 'static/data/review.csv'),
+            ('load_csv_comment', 'static/data/comments.csv'),
+        ]
+
+        for command, path in commands:
+            call_command(command, path=path)
+
+# python manage.py load_csv_all
+
