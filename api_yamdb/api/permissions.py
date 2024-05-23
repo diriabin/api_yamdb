68fe99d8dd4c84d1ad9f2280e9d7f7d37fd6fc38
Nikita Maslenko <nickita0098@gmail.com>
2024-05-23 12:38:59 +0200

    исправил ошибки 1-2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,25 +4,24 @@ from rest_framework import permissions
 class IsAdmin(permissions.IsAdminUser):
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated
-            and request.user.is_admin
+                request.user.is_authenticated
+                and request.user.is_admin
         )
 
 
 class IsAdminModeratorOwnerOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-            or request.user.is_moderator
-            or request.user.is_admin
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin
         )
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(IsAdmin):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_admin
+                super().has_permission(request, view)
+                or request.method in permissions.SAFE_METHODS
         )
