04a43a3ab4250c57b84a78b1161c1783208f9c07
Artem Diriabin <artem.diriabin@yandex.ru>
2024-05-24 17:08:15 +0700

    flake8

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,8 +1,8 @@
-from reviews.validators import validate_username, username_is_not_me
+from reviews.validators import validate_username, username_is_not_forbidden
 
 
 class UsernameMixin:
     def validate_username(self, username):
         validate_username(username)
-        username_is_not_me(username)
+        username_is_not_forbidden(username)
         return username
